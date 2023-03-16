69833bbaa793c32df280e010c8cc5f72b2f79ef1
dudagiglioli <dudaguilice@gmail.com>
2023-03-16 08:54:56 -0300

    BD :construction:

---
+++ src/main/resources/db/migration/V002__Criar_Tabela_Cidade.sql
@@ -0,0 +1,15 @@
+Create Table Cidade(
+idcidade int not null auto_increment primary key,
+nomecidade varchar(100),
+uf varchar(2),
+);
+
+insert into cidade(nomecidade, uf) value ('Botucatu', 'SP');
+insert into cidade(nomecidade, uf) value ('Salvador', 'BA');
+insert into cidade(nomecidade, uf) value ('Macatuba', 'SP');
+insert into cidade(nomecidade, uf) value ('Brasília', 'DF');
+insert into cidade(nomecidade, uf) value ('Nova Iguaçu', 'RJ');
+insert into cidade(nomecidade, uf) value ('Goiânia', 'GO');
+insert into cidade(nomecidade, uf) value ('Joinville', 'SC');
+
+
