6e5e9f333b5d2d1962c2a92fea51ce72a734dfa5
dudagiglioli <dudaguilice@gmail.com>
2023-05-18 08:54:30 -0300

    Filter and OrderBy :construction:

--- src/main/java/br/com/etec/dudag/cursoapi/repository/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/dudag/cursoapi/repository/curso/CursoRepositoryImpl.java
@@ -1,13 +1,14 @@
 package br.com.etec.dudag.cursoapi.repository.curso;
 
-import antlr.StringUtils;
 import br.com.etec.dudag.cursoapi.model.Curso;
 import br.com.etec.dudag.cursoapi.repository.filter.CursoFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -29,12 +30,22 @@ public class CursoRepositoryImpl implements CursoRepositoryQuery{
 
         Predicate[] predicates = criarRestricoes(cursoFilter, builder, root);
         criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecurso")));
+
+        TypedQuery<Curso> query = manager.createQuery(criteria);
+        //transformar o meu criteria em uma Query/consulta
+
         return null;
     }
 
     private Predicate[] criarRestricoes(CursoFilter cursoFilter, CriteriaBuilder builder, Root<Curso> root) {
         List<Predicate> predicates = new ArrayList<>(); // armazena os dados
 
-        if (StringUtils)
+        if (!StringUtils.isEmpty(cursoFilter.getNomecurso())){
+            predicates.add(builder.like(builder.lower(root.get("nomecurso")),
+                    "%" + cursoFilter.getNomecurso().toLowerCase() + "%" ));
+        }
+
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 }
